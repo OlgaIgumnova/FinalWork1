0a70d8e1b4318ab5e63075faf1229f8c7db6ce4f
Olga <o2igumn@gmail.ru>
2023-01-10 22:54:55 +0900

    Array

---
+++ Array/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] arr = {"Лето", "Осень", "Лед", "Сад", "Зима", "Ель", "Весна"};
+string[] result = new string[arr.Length];
+
+
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void Print(string[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.Write(mas[i]+" ");
+    }
+    Console.WriteLine();
+}
+
+NewArray(arr, result);
+Print(arr);
+Console.WriteLine();
+Print(result);
